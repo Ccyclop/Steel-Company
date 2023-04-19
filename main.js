71779f1fa97dc54b6a3b6d35a24e423198db6960
Ccyclop <tegi.miqautadze0@gmail.com>
2023-04-20 02:33:03 +0400

    added another page

--- main.js
+++ main.js
@@ -1,5 +1,40 @@
-const productsList = ['არმატურა', 'გლინულა', 'ზოლოვანა', 'კვადრატული მილი', 'მილი', 'მრგვალი რკინა', 'ორტესებრი კოჭი', 'შველერი', 'კუთხოვანა', 'სენდვიჩ პანელი', 'მასიური კვადრატი']
+const productsList = ['არმატურა', 'გლინულა', 'ზოლოვანა', 'კვადრატული მილი', 'მილი', 'მრგვალი რკინა', 'ორტესებრი კოჭი', 'შველერი', 'კუთხოვანა', 'სენდვიჩ პანელი', 'მასიური კვადრატი', 'ფურცლოვანა ფოლადი']
 const productsSrc = productsList.map((o) => `./assets/Produkcia/${o}.jpg`)
+
+window.addEventListener('load', () => {
+    if(window.location.href.includes('gallery.html')){
+        localStorage['page'] = 'gallery'
+    } else {
+        localStorage['page'] = 'index'
+        if(localStorage['scrollTo'] != null && localStorage['scrollTo'] != 'null') {
+            window.location.href += localStorage['scrollTo']
+            localStorage['scrollTo'] = null
+        }
+    }
+    if(localStorage['page'] == 'gallery'){
+        const catalogLink = document.querySelector('#catalog')
+        const contactLink = document.querySelector('#contactLink')
+
+        catalogLink.addEventListener('click', () => {
+            localStorage['scrollTo'] = '#catalogTitle'
+            window.location.href = window.location.href.replace('gallery', 'index')
+        })
+
+        contactLink.addEventListener('click', () => {
+            localStorage['scrollTo'] = '#contact'
+            window.location.href = window.location.href.replace('gallery', 'index')
+        })
+
+        catalogLink.removeEventListener('click', () => {
+            localStorage['scrollTo'] = '#catalogTitle'
+            window.location.href = window.location.href.replace('gallery', 'index')
+        })
+        contactLink.removeEventListener('click', () => {
+            localStorage['scrollTo'] = '#contact'
+            window.location.href = window.location.href.replace('gallery', 'index')
+        })
+    }
+})
 
 const productsPlace = document.querySelector('.products')
 
@@ -15,16 +50,20 @@ const addProduct = (src, name) => {
 }
 
 for(var i = 0; i < productsList.length; i++) {
-    addProduct(productsSrc[i], productsList[i])
+    try {
+        addProduct(productsSrc[i], productsList[i])
+    } catch {TypeError}
 }
 
 const header = document.querySelector('header')
 
 window.addEventListener('scroll', () => {
-    if(window.scrollY >= 900) {
-        header.style.backgroundColor = 'rgba(0,0,0,0.5)'
-    } else {
-        header.style.backgroundColor = 'transparent'
+    if(window.innerWidth > 630 && localStorage['page'] == 'index') {
+        if(window.scrollY >= 900) {
+            header.style.backgroundColor = 'rgba(0,0,0,0.5)'
+        } else {
+            header.style.backgroundColor = 'transparent'
+        }
     }
 })
 
